70d38e02080d057f0768638297db3ada14abae55
kevinfernaldy <kevin.fernaldy@gmail.com>
2019-02-19 16:36:29 +0700

    Revert "Added new fragments"
    This reverts commit b5d6410cdd60f5e9108a9d735b6d3e83f2644a0b.
    Wrong commit branch KevinF to Master

--- app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
+++ app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
@@ -1,51 +1,13 @@
 package com.example.tugasbesarandroid;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mEmail;
-    private TextView mUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null){
-            mEmail = (TextView) findViewById(R.id.main_email);
-            mUid = (TextView) findViewById(R.id.main_uid);
-
-            mEmail.setText(user.getEmail());
-            mUid.setText(user.getUid());
-        } else {
-            Intent intent = new Intent(this, LoginRegisterActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    }
-
-    public void signOut(View view){
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(this, LoginRegisterActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(MainActivity.class.getSimpleName(),"MainActivity destroyed");
     }
 }
