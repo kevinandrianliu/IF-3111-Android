f35ceeca1754eb8f1310dd30098ac43ec44c7fb7
kevinfernaldy <kevin.fernaldy@gmail.com>
2019-02-24 20:36:26 +0700

    Added firebase cloud messaging

--- app/src/main/java/com/example/tugasbesarandroid/HandleFirebaseMessaging.java
+++ app/src/main/java/com/example/tugasbesarandroid/HandleFirebaseMessaging.java
@@ -1,6 +1,21 @@
 package com.example.tugasbesarandroid;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class HandleFirebaseMessaging extends FirebaseMessagingService {
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.d(TAG, remoteMessage.getFrom());
+
+        if (remoteMessage.getNotification() != null){
+            Log.d(TAG, "Message notif body: " + remoteMessage.getNotification().getBody());
+        }
+    }
 }
--- app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
+++ app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tugasbesarandroid;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mEmail;
     private TextView mUid;
+    private Intent mService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
             mEmail.setText(user.getEmail());
             mUid.setText(user.getUid());
+
+            mService = new Intent(this,HandleFirebaseMessaging.class);
         } else {
             Intent intent = new Intent(this, LoginRegisterActivity.class);
             startActivity(intent);
@@ -41,6 +48,34 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginRegisterActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void subscribe(View view){
+        FirebaseMessaging.getInstance().subscribeToTopic("daily_quotes")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String message = "Task successful";
+                        if (!task.isSuccessful()){
+                            message = "Task not successful";
+                        }
+                        Log.d("MainActivity", message);
+                    }
+                });
+    }
+
+    public void unsubscribe(View view){
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("daily_quotes")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String message = "Task successful";
+                        if (!task.isSuccessful()){
+                            message = "Task not successful";
+                        }
+                        Log.d("MainActivity", message);
+                    }
+                });
     }
 
     @Override
