ab855c39a255ed00a93d9d9ffdd821e12ee0bfb5
belladj <belladestianajunaidi@gmail.com>
2019-02-25 14:05:40 +0700

    Added navigation bar profile and edit photo option

--- app/src/main/java/com/example/tugasbesarandroid/EditProfileActivity.java
+++ app/src/main/java/com/example/tugasbesarandroid/EditProfileActivity.java
@@ -1,14 +1,18 @@
 package com.example.tugasbesarandroid;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,43 +21,61 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.io.ByteArrayOutputStream;
+
 public class EditProfileActivity extends AppCompatActivity {
 
     private EditText mDisplayNameValue;
+    private Button mSelectPic;
     private Button mSaveChanges;
+    private ImageView mImage;
+    private Uri filePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         mDisplayNameValue = (EditText) findViewById(R.id.display_name_value);
         mSaveChanges = (Button) findViewById(R.id.save_changes);
+        mSelectPic = (Button) findViewById(R.id.upload_pic_button);
+        mImage = (ImageView) findViewById(R.id.image);
+
 
         mSaveChanges.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String mDisplayNameValueString = mDisplayNameValue.getText().toString();
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
                 if (user != null) {
 
-                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                            .setDisplayName(mDisplayNameValueString)
-                            .build();
+                    if (mDisplayNameValueString.matches("")) {
+                        Toast.makeText(getApplicationContext(), "Please enter a name", Toast.LENGTH_SHORT).show();
 
-                    user.updateProfile(profileUpdates)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Log.d(EditProfileActivity.class.getSimpleName(), "User profile updated.");
-                                        Toast.makeText(EditProfileActivity.this, "User profile updated", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                        startActivity(intent);
+                    } else {
+
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(mDisplayNameValueString)
+                                .setPhotoUri(filePath)
+                                .build();
+
+                        user.updateProfile(profileUpdates)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.d(EditProfileActivity.class.getSimpleName(), "User profile updated.");
+                                            Toast.makeText(EditProfileActivity.this, "User profile updated", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
                                     }
-                                }
-                            });
+                                });
+                        }
                     }
                     else {
                     Intent intent = new Intent(EditProfileActivity.this, LoginRegisterActivity.class);
@@ -62,7 +84,60 @@ public class EditProfileActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mSelectPic.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        Intent intent = new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                        intent.setType("image/*");
+                        intent.putExtra("crop", "true");
+                        intent.putExtra("scale", true);
+                        intent.putExtra("outputX", 100);
+                        intent.putExtra("outputY", 100);
+                        intent.putExtra("aspectX", 1);
+                        intent.putExtra("aspectY", 1);
+                        intent.putExtra("return-data", true);
+                        startActivityForResult(intent, 1);
+                    }
+                });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == 1) {
+                filePath = data.getData();
+
+            try
+            {
+                Bitmap newProfilePic = MediaStore.Images.Media.getBitmap(this.getContentResolver(), filePath);
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                newProfilePic.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                mImage.setImageBitmap(newProfilePic);
+            }
+            catch (Exception e)
+            {
+            }
+
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
--- app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
+++ app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
@@ -1,43 +1,99 @@
 package com.example.tugasbesarandroid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
     private TextView mEmail;
+    private TextView mEmailNavbar;
     private TextView mUid;
     private TextView mDisplayName;
+    private ImageView mImage;
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
+    private NavigationView mNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawer = (DrawerLayout)findViewById(R.id.activity_main);`
+        mToggle = new ActionBarDrawerToggle(this, mDrawer,R.string.drawer_open, R.string.drawer_close);
+
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNavigation = (NavigationView)findViewById(R.id.navigation_view);
+        View mNavView = mNavigation.getHeaderView(0);
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null){
             mEmail = (TextView) findViewById(R.id.main_email);
+            mEmailNavbar = (TextView) mNavView.findViewById(R.id.email);
             mUid = (TextView) findViewById(R.id.main_uid);
-            mDisplayName = (TextView) findViewById(R.id.display_name);
+            mDisplayName = (TextView) mNavView.findViewById(R.id.display_name);
+            mImage = (ImageView) mNavView.findViewById(R.id.image);
 
             mEmail.setText(user.getEmail());
+            mEmailNavbar.setText(user.getEmail());
             mUid.setText(user.getUid());
             mDisplayName.setText(user.getDisplayName());
+            Picasso.with(this).load(user.getPhotoUrl()).placeholder(R.drawable.icon).into(mImage);
 
         } else {
             Intent intent = new Intent(this, LoginRegisterActivity.class);
             startActivity(intent);
             finish();
         }
+
+        mNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), EditProfileActivity.class));
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void signOut(View view){
