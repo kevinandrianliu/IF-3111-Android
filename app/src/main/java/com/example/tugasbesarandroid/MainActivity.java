6ffd035040a016d03e8d436d4b2ce50e9f43cb20
belladj <belladestianajunaidi@gmail.com>
2019-02-25 20:02:25 +0700

    added linear accel to check whether someone is running or walking

--- app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
+++ app/src/main/java/com/example/tugasbesarandroid/MainActivity.java
@@ -21,12 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private double stepThreshold = 10.5;
     private final String TAG = this.getClass().getSimpleName();
     private Sensor accelerometerSensor;
-    private Sensor gyroscopeSensor;
+    private Sensor linearAcceleration;
 
-    private TextView gyroscopeValue;
+    private TextView lineaccValue;
     private TextView accelerometerValue;
     private TextView stepCount;
     private EditText thresholdValue;
+    private TextView mMovementType;
 
     private long timeBefore;
     private int steps;
@@ -40,10 +41,11 @@ public class MainActivity extends AppCompatActivity {
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        linearAcceleration = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
-        gyroscopeValue = (TextView) findViewById(R.id.gyroscope_value);
+        lineaccValue = (TextView) findViewById(R.id.accel_value);
         accelerometerValue = (TextView) findViewById(R.id.accelerometer_value);
+        mMovementType = (TextView) findViewById(R.id.type);
         stepCount = (TextView) findViewById(R.id.step_count);
         thresholdValue = (EditText) findViewById(R.id.threshold_value);
 
@@ -52,32 +54,38 @@ public class MainActivity extends AppCompatActivity {
         sensorManager.registerListener(new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                    double q0 = event.values[3];
-                    double q1 = event.values[0];
-                    double q2 = event.values[1];
-                    double q3 = event.values[2];
+                long timeNow = event.timestamp;
 
-                    double x = atan2(2.0 * (q0 * q1 + q2 * q3), 1.0 - 2.0 * (q1 * q1 + q2 * q2));
-                    double y = asin(2.0 * (q0 * q2 - q3 * q1));
-                    double z = atan2(2.0 * (q0 * q3 + q1 * q2), 1.0 - 2.0 * (q2 * q2 + q3 * q3));
+                if (timeBefore == 0){
+                    timeBefore = timeNow;
+                } else {
+                    if (timeNow - timeBefore > 100000000L) {
+                        timeBefore = timeNow;
+                        String text =
+                                "X = " + ((Float) event.values[0]).toString() + "\n" +
+                                        "Y = " + ((Float) event.values[1]).toString() + "\n" +
+                                        "Z = " + ((Float) event.values[2]).toString() + "\n";
 
-                    x = x * 180 / PI;
-                    y = y * 180 / PI;
-                    z = z * 180 / PI;
+                        double magnitude = sqrt(event.values[0]*event.values[0] + event.values[1]*event.values[1] + event.values[2]*event.values[2]);
 
-                    String text =
-                            "X = " + ((Double) x).toString() + "\n" +
-                            "Y = " + ((Double) y).toString() + "\n" +
-                            "Z = " + ((Double) z).toString() + "\n";;
+                        lineaccValue.setText(text);
 
-                    gyroscopeValue.setText(text);
+
+                        if (magnitude >= 10) {
+                            mMovementType.setText("RUN");
+                        } else {
+                            mMovementType.setText("WALK");
+                        }
+
+                    }
+                }
             }
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
             }
-        }, gyroscopeSensor, SensorManager.SENSOR_DELAY_UI);
+        }, linearAcceleration, SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
